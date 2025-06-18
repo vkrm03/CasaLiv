b4f1b780afc86afcf8af62a0b92728df0aeec218
vikram <vikramrokith03@gmail.com>
2025-06-19 00:14:59 +0530

    DB Bug fixed

--- backend/server.js
+++ backend/server.js
@@ -11,10 +11,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => console.log("MongoDB Connected"))
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
   .catch(err => console.error("Mongo Error:", err));
 
 const JWT_SECRET = process.env.JWT_SECRET || 'casalivsecretkey';
